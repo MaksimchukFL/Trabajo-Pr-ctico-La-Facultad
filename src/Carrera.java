4e7d7025a04d5a70e4246285e13e40e56bbdf827
Matias Barboza <matiasbarboza540@gmail.com>
2022-09-24 18:38:07 -0300

    Desarrollo de persona y carrera

--- src/Carrera.java
+++ src/Carrera.java
@@ -1,2 +1,43 @@
+import java.util.LinkedList;
+
 public class Carrera {
+    private String nombre;
+    private LinkedList<Materia> materias;
+
+    public Carrera(String nombre){
+        this.nombre = nombre;
+        materias = new LinkedList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Materia> getMaterias() {
+        return materias;
+    }
+
+    public void agregarMateria(Materia materia){
+        materias.add(materia);
+    }
+
+    public void eliminarMateria(String nombreMateria){
+        for(Materia materia: materias){
+            if(materia.getNombre() == nombreMateria){
+                materias.remove(materia);
+            }
+        }
+    }
+
+    public void encontrarMateria(String nombreMateria){
+        for(Materia materia: materias){
+            if(materia.getNombre() == nombreMateria){
+                System.out.println("La materia se encuentra en la carrera");
+            }
+        }
+    }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,70 @@
-public abstract class Persona {
+import java.util.Scanner;
 
+public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private int legajo;
+
+    public Persona(String nombre,String apellido,int legajo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public void modificarDatos(){
+        Scanner leer = new Scanner(System.in);
+        int opcion,legajo;
+        String nombre,apellido;
+
+        System.out.println("¿Que desea modificar?");
+        System.out.println("1. Nombre \n2.Apellido \n3.Legajo");
+        opcion = leer.nextInt();
+        leer.nextLine();
+
+        if(opcion == 1){
+            System.out.println("Ingrese el nombre para modificar: ");
+            nombre = leer.nextLine();
+            setNombre(nombre);
+            System.out.println("Cambio hecho");
+            System.out.println(getApellido() + ", " + getNombre());
+        }else if(opcion == 2){
+            System.out.println("Ingrese el apellido para modificar: ");
+            apellido = leer.nextLine();
+            setApellido(apellido);
+            System.out.println("Cambio hecho");
+            System.out.println(getApellido() + ", " + getNombre());
+        }else if(opcion == 3){
+            System.out.println("Ingrese el legajo para modificar: ");
+            legajo = leer.nextInt();
+            setLegajo(legajo);
+            System.out.println("Cambio hecho");
+            System.out.println(getLegajo());
+        }else{
+            System.out.println("Opcion invalida");
+        }
+    }
 }
