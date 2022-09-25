30d682ff7b9d7d3190ea46073fa004b5076a1c89
JoseAlegre69 <joseignacioalegre4@gmail.com>
2022-09-25 00:53:13 -0300

    Materia&Estudiante

--- src/Materia.java
+++ src/Materia.java
@@ -1,3 +1,55 @@
+import java.util.LinkedList;
+
 public class Materia {
+    private String nombre;
+    private Profesor titular;
+    private LinkedList<Estudiante> coleccionEstudiantes = new LinkedList<>();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Profesor getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Profesor titular) {
+        this.titular = titular;
+    }
+
+    public Materia(String nombre, Profesor profesor){
+        this.nombre = nombre;
+        this.titular = profesor;
+    }
+
+    public void eliminarEstudiante(int legajo)
+    {
+        for (Estudiante estudiante : coleccionEstudiantes){
+            if(estudiante.getLegajo()==legajo){
+                coleccionEstudiantes.remove(estudiante);
+            }
+        }
+    }
+    public void modificarTitular(Profesor profesor)
+    {
+        this.titular = profesor;
+    }
+    public void agregarEstudiante(Estudiante estudiante)
+    {
+        coleccionEstudiantes.add(estudiante);
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" +
+                "nombre='" + nombre + '\'' +
+                ", titular=" + titular +
+                ", coleccionEstudiantes=" + coleccionEstudiantes +
+                '}';
+    }
 
 }
